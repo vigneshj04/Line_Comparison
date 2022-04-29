afcbac548a0a7d8f96f3f711a524a37e19d17c98
vigneshj04 <vigneshjmax@gmail.com>
2022-04-29 08:03:19 +0530

    Added: Finding Length Of Two Lines

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
+
+        double lengthLine = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the value of x1: ");
+        double x1 = sc.nextDouble();
+        System.out.println("Enter the value of x2: ");
+        double x2 = sc.nextDouble();
+
+        System.out.println("Enter the value of y1: ");
+        double y1 = sc.nextDouble();
+        System.out.println("Enter the value of y2: ");
+        double y2 = sc.nextDouble();
+
+        lengthLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of line is: " + lengthLine);
     }
 }
